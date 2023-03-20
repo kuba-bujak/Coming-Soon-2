43168a6279e7b5bdd4f7fda9cbd7b305221e4d91
Jakub Bujak <92362918+kuba-bujak@users.noreply.github.com>
2023-03-20 11:14:56 +0100

    Update send.php

--- send.php
+++ send.php
@@ -14,8 +14,8 @@ try {
 	$mail->isSMTP();                                            //Send using SMTP
 	$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-	$mail->Username   = 'kubab200012@gmail.com';                     //SMTP username
-	$mail->Password   = 'popvbxrixttknisp';                               //SMTP password
+	$mail->Username   = 'username';                     //SMTP username
+	$mail->Password   = 'password';                               //SMTP password
 	$mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
 	$mail->Port       = 465;                                    //TCP port to connect to; use 587
 
